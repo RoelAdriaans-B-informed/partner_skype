e8b417b4690611e459db2a52847be78e73dd8dee
Roel Adriaans <roel.adriaans@b-informed.nl>
2015-03-18 21:48:23 +0100

    [ADD] First import partner_skype module

---
+++ __openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Roel Adriaans - B-Informed B.V.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Extend partner with skype address',
+  'description': """
+Extend partner with skype address
+=================================
+
+""",
+ 'version': '1.0',
+ 'author': 'B-Informed',
+ 'maintainer': 'B-Informed',
+ 'category': 'Extra Tools',
+ 'website': 'http://www.b-informed.nl/',
+ 'depends': ['base'],
+ 'data': [ 'partner_extend.xml'],
+ 'demo': [],
+ 'test': [],
+ 'auto_install': False,
+ 'installable': True,
+ 'images': []
+}
